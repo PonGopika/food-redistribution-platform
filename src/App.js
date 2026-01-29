df5904f0d8c692cf2f6f081ef5e941f8963b227c
Sivadharaneesh <sivadharaneesh017@gmail.com>
2026-01-29 22:50:36 +0530

    Initial commit in front rnd branch

--- src/App.js
+++ src/App.js
@@ -1,46 +1,71 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import { FoodProvider } from "./context/FoodContext";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import RoleSelection from "./pages/auth/RoleSelection";
 import DonorDashboard from "./pages/donor/DonorDashboard";
-import NGODashboard from "./pages/ngo/NGODashboard";
-import VolunteerDashboard from "./pages/volunteer/VolunteerDashboard";
+import DonorVerification from "./pages/donor/DonorVerification";
 import AddFood from "./pages/donor/AddFood";
 import FoodStatus from "./pages/donor/FoodStatus";
+import NGODashboard from "./pages/ngo/NGODashboard";
+import NGOProfile from "./pages/ngo/NGOProfile";
 import NearbyFood from "./pages/ngo/NearbyFood";
 import AcceptedFood from "./pages/ngo/AcceptedFood";
+import VolunteerDashboard from "./pages/volunteer/VolunteerDashboard";
+import VolunteerOnboarding from "./pages/volunteer/VolunteerOnboarding";
 import PickupTasks from "./pages/volunteer/PickupTasks";
+import ConfirmDelivery from "./pages/volunteer/ConfirmDelivery";
+import DeliverySuccess from "./pages/volunteer/DeliverySuccess";
 import DeliveryHistory from "./pages/volunteer/DeliveryHistory";
+import VolunteerBadges from "./pages/volunteer/VolunteerBadges";
 import AdminDashboard from "./pages/admin/AdminDashboard";
+import ProfileDashboard from "./pages/profile/ProfileDashboard";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/role" element={<RoleSelection />} />
-        <Route path="/donor" element={<DonorDashboard />} />
-        <Route path="/ngo" element={<NGODashboard />} />
-        <Route path="/volunteer" element={<VolunteerDashboard />} />
-        {/* Donor routes*/}
-        <Route path="/donor/add-food" element={<AddFood />} />
-        <Route path="/donor/status" element={<FoodStatus />} />
+    <AuthProvider>
+      <FoodProvider>
+        <BrowserRouter>
+          <Routes>
+            {/* Auth routes */}
+            <Route path="/" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/role" element={<RoleSelection />} />
 
-        {/* NGO routes*/}
-        <Route path="/ngo/nearby-food" element={<NearbyFood />} />
-        <Route path="/ngo/accepted" element={<AcceptedFood />} />
+            {/* Profile */}
+            <Route path="/profile" element={<ProfileDashboard />} />
 
-        {/* Volunteer routes*/}
-        <Route path="/volunteer/tasks" element={<PickupTasks />} />
-        <Route path="/volunteer/history" element={<DeliveryHistory />} />
-        
+            {/* Donor routes */}
+            <Route path="/donor" element={<DonorDashboard />} />
+            <Route path="/donor/add-food" element={<AddFood />} />
+            <Route path="/donor/status" element={<FoodStatus />} />
+            <Route path="/donor/verification" element={<DonorVerification />} />
 
-        <Route path="/admin/dashboard" element={<AdminDashboard />} />
+            {/* NGO routes */}
+            <Route path="/ngo" element={<NGODashboard />} />
+            <Route path="/ngo/nearby-food" element={<NearbyFood />} />
+            <Route path="/ngo/accepted" element={<AcceptedFood />} />
+            <Route path="/ngo/profile" element={<NGOProfile />} />
 
-      </Routes>
-    </BrowserRouter>
+            {/* Volunteer routes */}
+            <Route path="/volunteer" element={<VolunteerDashboard />} />
+            <Route path="/volunteer/onboarding" element={<VolunteerOnboarding />} />
+            <Route path="/volunteer/tasks" element={<PickupTasks />} />
+            <Route path="/volunteer/confirm-delivery/:id" element={<ConfirmDelivery />} />
+            <Route path="/volunteer/delivery-success" element={<DeliverySuccess />} />
+            <Route path="/volunteer/history" element={<DeliveryHistory />} />
+            <Route path="/volunteer/badges" element={<VolunteerBadges />} />
+
+            {/* Admin routes */}
+            <Route path="/admin" element={<AdminDashboard />} />
+            <Route path="/admin/dashboard" element={<AdminDashboard />} />
+          </Routes>
+        </BrowserRouter>
+      </FoodProvider>
+    </AuthProvider>
   );
 }
 
 export default App;
+
